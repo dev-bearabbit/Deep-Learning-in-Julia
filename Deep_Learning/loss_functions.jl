13ff483e0e7a66e09a2944bf2e652af6d92e60c3
Hyeonji ryu <42489770+Hyeonji-Ryu@users.noreply.github.com>
2020-04-12 05:24:55 +0900

    Update loss_functions.jl

--- Deep_Learning/loss_functions.jl
+++ Deep_Learning/loss_functions.jl
@@ -1,10 +1,19 @@
+# 평균 제곱 오차
 function mean_squared_error(y,t)
     error=(y-t).^2
     return sum(error)/length(y)
 end
 
-
+# 교차 엔트로피 오차 (single)
 function cross_entropy_error(y,t)
     delta=1e-7
     return -sum(log.(y.+delta).*t)
+end
+
+# 교차 엔트로피 오차 (batch)
+
+function cross_entropy_error(y,t)
+    delta = 1e-7
+    batch_size = y[:,1]
+    return (-sum(log.(y.+delta).*t)/batch_size)
 end
