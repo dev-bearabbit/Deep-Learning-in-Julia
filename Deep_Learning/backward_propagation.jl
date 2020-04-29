08a52516311a972a8f82607656ead9595122364f
Hyeonji ryu <42489770+Hyeonji-Ryu@users.noreply.github.com>
2020-04-30 01:20:13 +0900

    Update backward_propagation.jl

--- Deep_Learning/backward_propagation.jl
+++ Deep_Learning/backward_propagation.jl
@@ -3,22 +3,19 @@
 """
 TwoLayerNet를 mutable struct로 만듭니다.
 """
-mutable struct init_network
-    W1
-    b1
-    W2
-    b2
-end
+params = Dict()
+grads = Dict()
 
 function making_network(input_size, hidden_size, output_size, weight_init_std =0.01)
-    W1 = weight_init_std * randn(Float64, input_size, hidden_size)
-    b1 = zeros(Float32, 1, hidden_size)
-    W2 = weight_init_std * randn(Float64, hidden_size, output_size)
-    b2 = zeros(Float32, 1, output_size)
-    return(init_network(W1, b1, W2, b2))
+    params["W1"] = weight_init_std * randn(Float64, input_size, hidden_size)
+    params["b1"] = zeros(Float32, 1, hidden_size)
+    params["W2"] = weight_init_std * randn(Float64, hidden_size, output_size)
+    params["b2"] = zeros(Float32, 1, output_size)
+    return(params)
 end
 
-TwoLayerNet = making_network(784, 50, 10)
+making_network(784, 50, 10)
+
 
 ##### 역전파 인스턴스 설정 ##### 
 
@@ -49,7 +46,7 @@ result = SoftmaxwithLoss(0,0)
 dense1 = dense_layer(0,0,0,0,0)
 dense2 = dense_layer(0,0,0,0,0)
 Sigmoid1 = Sigmoid(0)
-# Relu = ReLu(0)
+Relu = ReLu(0)
 
 ##### 역전파에 필요한 함수 정의 #####
 
@@ -124,6 +121,13 @@ function relu_backward(Relu,dout)
     return dx
 end
 
+function SGD(params,grads)
+    for key in keys(params)
+        params[key] -= grads[key]
+    end
+    return params
+end
+
 ##### 역전파에 필요한 변수 정의 #####
 
 train_loss_list = Float64[]
@@ -131,32 +135,34 @@ accuracy = Float64[]
 train_size = size(train_x)[1]
 batch_size = 100
 learning_rate = 0.1
+iters_num = 600
 
 ##### 역전파 알고리즘 코드 ##### 
 
 @time begin
-    for i in 1: 600
+    for i in 1: iters_num
         batch_mask = rand(1:train_size, 100)
         x_batch = train_x[batch_mask, :]
         t_batch = t[batch_mask, :]
         
         # 순전파
-        z1 = dense_layer_forward(dense1,x_batch,TwoLayerNet.W1,TwoLayerNet.b1)
+        z1 = dense_layer_forward(dense1,x_batch,params["W1"],params["b1"])
         a1 = sigmoid_forward(Sigmoid1,z1)
-        z2 = dense_layer_forward(dense2,a1,TwoLayerNet.W2,TwoLayerNet.b2)
+        z2 = dense_layer_forward(dense2,a1,params["W2"],params["b2"])
         num = SoftmaxwithLoss_forward(z2,t_batch)
-    
+        
         # 역전파
         last_layer = SoftmaxwithLoss_backward(result)
         z2_back = dense_layer_backward(dense2, last_layer)
+        grads["W2"] = dense2.dw
+        grads["b2"] = dense2.db
         a1_back = sigmoid_backward(Sigmoid1, z2_back)
         z1_back = dense_layer_backward(dense1, a1_back)
+        grads["W1"] = dense1.dw
+        grads["b1"] = dense1.db
 
-        # 가중치 갱신
-        TwoLayerNet.W1 -= (learning_rate * dense1.dw)
-        TwoLayerNet.W2 -= (learning_rate * dense2.dw)
-        TwoLayerNet.b1 -= (learning_rate * dense1.db)
-        TwoLayerNet.b2 -= (learning_rate * dense2.db)
+        #가중치 갱신 (SGD)
+        SGD(params, grads)
     
         temp_loss = loss(x_batch, t_batch)
         print("NO.$i: ")
