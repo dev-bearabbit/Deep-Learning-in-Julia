b0aff2e64fd448a6b94cbac4b1d11d502a59e25e
Hyeonji ryu <42489770+Hyeonji-Ryu@users.noreply.github.com>
2020-06-18 21:43:43 +0900

    Update weight_initialization.jl

--- MLP/weight_initialization.jl
+++ MLP/weight_initialization.jl
@@ -14,35 +14,36 @@ weight_size: 은닉층의 사이즈 ( 배열로 구성 )
 input_size: 입력데이터의 사이즈 ( ex. “MNIST” 예시에서는 (1, 784) )
 weight_init: 초기값 설정 ( “std”, “Xavier”, “He” 중 선택 )
 ```
-
+function making_network(W, b, weight_size, input_size, weight_init)
+    
     params = Dict()
-
+    
     if weight_init == "std"
         for i in (1:length(W))
             params[W[i]] = 0.01 * randn(Float64, weight_size[i])
             params[b[i]] = zeros(Float32,1, weight_size[i][end])
         end
         return(params)
-
+        
     elseif weight_init == "Xavier"
         for i in (1:length(W))
             if i == 1
                 params[W[i]] = ((1.0 / prod(input_size))^(1/2)) * randn(Float64, weight_size[i])
                 params[b[i]] = zeros(Float32,1, weight_size[i][end])
             else
-                params[W[i]] = ((1.0 / (prod(weight_size[i-1])+weight_size[i-1][end]))^(1/2)) * randn(Float64, weight_size[i])
+                params[W[i]] = ((1.0 / (prod(weight_size[i-1][1:2])* weight_size[i-1][end]))^(1/2)) * randn(Float64, weight_size[i])
                 params[b[i]] = zeros(Float32,1, weight_size[i][end])
             end
         end
         return(params)
-
+        
     elseif weight_init == "He"
         for i in (1:length(W))
             if i == 1
                 params[W[i]] = ((2.0 / prod(input_size))^(1/2)) * randn(Float64, weight_size[i])
                 params[b[i]] = zeros(Float32,1, weight_size[i][end])
             else
-                params[W[i]] = ((2.0 / (prod(weight_size[i-1])+ weight_size[i-1][end]))^(1/2)) * randn(Float64, weight_size[i])
+                params[W[i]] = ((2.0 / (prod(weight_size[i-1][1:2])* weight_size[i-1][end]))^(1/2)) * randn(Float64, weight_size[i])
                 params[b[i]] = zeros(Float32,1, weight_size[i][end])
             end
         end
@@ -50,7 +51,6 @@ weight_init: 초기값 설정 ( “std”, “Xavier”, “He” 중 선택 )
     end
     return(params)
 end
-
 
 # 사용법
 
