ed4ac912db8f2f22e712008b7363cf98e0b0b687
Hyeonji ryu <42489770+Hyeonji-Ryu@users.noreply.github.com>
2020-06-24 14:46:28 +0900

    Update backward_propagation.jl

--- MLP/backward_propagation.jl
+++ MLP/backward_propagation.jl
@@ -14,125 +14,13 @@ end
 making_network(784, 50, 10)
 
 
-##### 역전파 인스턴스 설정 ##### 
-
-mutable struct dense_layer
-    x
-    w
-    b
-    dw
-    db
-end
-
-mutable struct Sigmoid
-    z
-end
-
-mutable struct ReLu
-    mask
-end
-
-mutable struct SoftmaxwithLoss
-    y
-    t
-end
-
-# 계층마다 인스턴스를 만들어줘야 한다.
+##### 역전파 인스턴스 설정 #####
 
 result = SoftmaxwithLoss(0,0)
 dense1 = dense_layer(0,0,0,0,0)
 dense2 = dense_layer(0,0,0,0,0)
 Sigmoid1 = Sigmoid(0)
 Relu = ReLu(0)
-
-##### 역전파에 필요한 함수 정의 #####
-
-function cross_entropy_error(y,t)
-    delta = 1e-7
-    batch_size = length(y[:,1])
-    return (-sum(log.(y.+delta).*t) / batch_size)
-end
-
-function sigmoid(x)
-    return 1/(1+exp(-x))
-end
-
-function relu(x)
-    return max(0,x)
-end
-
-function softmax_single(a)
-    c = maximum(a)
-    exp.(a .- c) / sum(exp.(a .- c))
-end
-
-function softmax(a)
-    temp = map(softmax_single, eachrow(a))
-    return(transpose(hcat(temp ...)))
-end
-
-function SoftmaxwithLoss_forward(x,t)
-    y = softmax(x)
-    loss = cross_entropy_error(y, t)
-    result.y = y
-    result.t = t
-    return loss
-end
-
-function SoftmaxwithLoss_backward(result,dout=1)
-    batch_size = size(result.t)[1]
-    dx = (result.y-result.t) / batch_size
-    return dx
-end
-
-function dense_layer_forward(dense,x,w,b)
-    cal = (x * w) .+ b
-    dense.x = x
-    dense.w = w
-    dense.b = b
-    return cal
-end
-
-function dense_layer_backward(dense, dout)
-    dx = *(dout,Array(dense.w'))
-    dense.dw = *(Array(dense.x'), dout)
-    dense.db = Array(sum(eachrow(dout))')
-    return dx
-end
-
-function sigmoid_forward(Sigmoid, x)
-    dx = sigmoid.(x)
-    Sigmoid.z = dx
-    return dx
-end
-
-function sigmoid_backward(Sigmoid, dout)
-    dx = dout .* (1.0 .- Sigmoid.z) .* Sigmoid.z
-    return dx
-end
-
-function relu_forward(Relu, x)
-    x = relu.(x)
-    Relu.mask = x .> 0
-    return x
-end
-
-function relu_backward(Relu,dout)
-    dx = dout.* Relu.mask
-    return dx
-end
-
-function evaluate(test_x,test_y)
-    temp = (sum((argmax.(eachrow(predict(test_x))).-1) .== test_y)/size(test_x)[1])
-    return (temp * 100)
-end
-
-function SGD(params,grads)
-    for key in keys(params)
-        params[key] -= learning_rate * grads[key]
-    end
-    return params
-end
 
 ##### 역전파에 필요한 변수 정의 #####
 
@@ -143,7 +31,7 @@ batch_size = 100
 learning_rate = 0.1
 iters_num = 600
 
-##### 역전파 알고리즘 코드 ##### 
+##### 역전파 알고리즘 코드 #####
 
 @time begin
     for i in 1: iters_num
