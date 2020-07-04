9f25b849306c43c66d3858a9635e4cc26bdfff92
Hyeonji ryu <42489770+Hyeonji-Ryu@users.noreply.github.com>
2020-07-04 15:59:19 +0900

    Update weight_initialization.jl

--- MLP/weight_initialization.jl
+++ MLP/weight_initialization.jl
@@ -1,8 +1,8 @@
 # 신경망 층에 따른 가중치와 편향 설정
 
-W = ["W1", "W2", "W3"]
-b = ["b1", "b2", "b3"]
-input_size = (1, 784)
+W = ["W1", "W2"]
+b = ["b1", "b2"]
+output_shape = [(1, 784), (1,50)]
 weight_size = [(784, 50), (50,10)]
 
 # 네트워크 생성하는 함수 정의
@@ -11,42 +11,32 @@ weight_size = [(784, 50), (50,10)]
 W: 신경망에 필요한 가중치 ( 배열로 구성, ex. 2층 신경망 = [“W1”, “W2”] )
 b: 신경망에 필요한 편향 ( 배열로 구성, ex. 2층 신경망 = [“b1”, “b2”] )
 weight_size: 은닉층의 사이즈 ( 배열로 구성 )
-input_size: 입력데이터의 사이즈 ( ex. “MNIST” 예시에서는 (1, 784) )
+output_shape: 각 층의 결과값 사이즈 ( 배열로 구성 )
 weight_init: 초기값 설정 ( “std”, “Xavier”, “He” 중 선택 )
 ```
 
-function making_network(W, b, weight_size, input_size, weight_init)
+function making_network(W, b, weight_size, output_shape, weight_init)
     
     params = Dict()
     
     if weight_init == "std"
         for i in (1:length(W))
             params[W[i]] = 0.01 * randn(Float64, weight_size[i])
-            params[b[i]] = zeros(Float32,1, weight_size[i][end])
+            params[b[i]] = zeros(Float64,1, weight_size[i][end])
         end
         return(params)
         
     elseif weight_init == "Xavier"
         for i in (1:length(W))
-            if i == 1
-                params[W[i]] = ((1.0 / prod(input_size))^(1/2)) * randn(Float64, weight_size[i])
-                params[b[i]] = zeros(Float32,1, weight_size[i][end])
-            else
-                params[W[i]] = ((1.0 / (prod(weight_size[i-1])+weight_size[i-1][end]))^(1/2)) * randn(Float64, weight_size[i])
-                params[b[i]] = zeros(Float32,1, weight_size[i][end])
-            end
+            params[W[i]] = ((1.0 / prod(output_shape[i]))^(1/2)) * randn(Float64, weight_size[i])
+            params[b[i]] = zeros(Float64,1, weight_size[i][end])
         end
         return(params)
         
     elseif weight_init == "He"
         for i in (1:length(W))
-            if i == 1
-                params[W[i]] = ((2.0 / prod(input_size))^(1/2)) * randn(Float64, weight_size[i])
-                params[b[i]] = zeros(Float32,1, weight_size[i][end])
-            else
-                params[W[i]] = ((2.0 / (prod(weight_size[i-1])+ weight_size[i-1][end]))^(1/2)) * randn(Float64, weight_size[i])
-                params[b[i]] = zeros(Float32,1, weight_size[i][end])
-            end
+            params[W[i]] = ((2.0 /prod(output_shape[i]))^(1/2)) * randn(Float64, weight_size[i])
+            params[b[i]] = zeros(Float64,1, weight_size[i][end])
         end
         return(params)
     end
@@ -55,4 +45,4 @@ end
 
 # 사용법
 
-params = making_network(W, b, weight_size, input_size, "He")
+params = making_network(W, b, weight_size, output_shape, "He")
